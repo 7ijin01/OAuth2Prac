d230a29549e9b4ae9a00a8a1c7bdead86ec6dd79
웃기진않아 <dugudada01@naver.com>
2025-03-25 12:29:05 +0900

    FEAT: OAuth2User 구현

--- src/main/java/org/example/oauth2/oAuth2/service/CustomOAuth2UserService.java
+++ src/main/java/org/example/oauth2/oAuth2/service/CustomOAuth2UserService.java
@@ -3,6 +3,8 @@ package org.example.oauth2.oAuth2.service;
 import org.example.oauth2.oAuth2.dto.GoogleResponse;
 import org.example.oauth2.oAuth2.dto.NaverResponse;
 import org.example.oauth2.oAuth2.dto.OAuth2Response;
+import org.example.oauth2.oAuth2.dto.user.CustomOAuth2User;
+import org.example.oauth2.oAuth2.dto.user.UserDto;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -15,6 +17,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
+        //userRequest는 리소스 서버에서 발급받아짐
 
         System.out.println(oAuth2User);
 
@@ -32,6 +35,13 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService
 
             return null;
         }
+        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
 
+        UserDto userDTO = new UserDto();
+        userDTO.setUsername(username);
+        userDTO.setName(oAuth2Response.getName());
+        userDTO.setRole("ROLE_USER");
+
+        return new CustomOAuth2User(userDTO);
     }
 }
