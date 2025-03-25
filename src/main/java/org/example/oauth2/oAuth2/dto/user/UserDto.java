595c56a9816a0321ab4c20789470775024ff3d95
웃기진않아 <dugudada01@naver.com>
2025-03-25 12:28:51 +0900

    FEAT: OAuth2User 구현

---
+++ src/main/java/org/example/oauth2/oAuth2/dto/user/UserDto.java
@@ -0,0 +1,13 @@
+package org.example.oauth2.oAuth2.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+
+    private String role;
+    private String name;
+    private String username;
+}
