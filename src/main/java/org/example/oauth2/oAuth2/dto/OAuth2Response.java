97f058e23f24ee2271e95424d71fac1bda312a68
웃기진않아 <dugudada01@naver.com>
2025-03-25 12:10:32 +0900

    FEAT: OAuth2Response 구현(naver,google)

---
+++ src/main/java/org/example/oauth2/oAuth2/dto/OAuth2Response.java
@@ -0,0 +1,13 @@
+package org.example.oauth2.oAuth2.dto;
+
+public interface OAuth2Response
+{
+
+    String getProvider();
+
+    String getProviderId();
+
+    String getEmail();
+
+    String getName();
+}
